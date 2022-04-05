0ec0f21b7c52b4c8e131a3fc6e4c7a68dcc65e78
Simon Knopp <s.j.knopp@gmail.com>
2022-04-06 10:22:34 +1200

    Disable port dropdown when running

--- picova-ui/ViewModels/MainWindowViewModel.cs
+++ picova-ui/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.ObjectModel;
 using System.IO.Ports;
 using System.Linq;
@@ -18,6 +18,7 @@ namespace PicovaUI.ViewModels
         [Reactive] public string? SelectedPort { get; set; }
         [ObservableAsProperty] public string RunLabel { get; } = string.Empty;
         public ReactiveCommand<Unit, Unit> Run { get; }
+        [ObservableAsProperty] public bool Running { get; }
         public MeasurementPlotViewModel MeasurementPlot { get; } = new();
 
         public double WindowSeconds
@@ -44,13 +45,16 @@ namespace PicovaUI.ViewModels
             });
 
             this.WhenAnyValue(vm => vm.reader.Connected)
-                .Select(con => con ? "Stop" : "Run")
+                .ToPropertyEx(this, vm => vm.Running);
+
+            this.WhenAnyValue(vm => vm.Running)
+                .Select(running => running ? "Stop" : "Run")
                 .ToPropertyEx(this, vm => vm.RunLabel);
 
             reader.Measurements
                 .Buffer(TimeSpan.FromMilliseconds(100))
                 .ObserveOn(AvaloniaScheduler.Instance)
-                .Where(_ => reader.Connected)
+                .Where(_ => Running)
                 .Subscribe(MeasurementPlot.AddMeasurements);
         }
     }
